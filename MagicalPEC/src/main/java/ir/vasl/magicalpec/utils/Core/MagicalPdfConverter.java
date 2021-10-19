b8b0a1501405f7e187e5d1ade0950f2e2f9349c1
M Odd <emad.mollaei@gmail.com>
2021-10-19 14:05:00 +0330

    new method for image conversion

--- MagicalPEC/src/main/java/ir/vasl/magicalpec/utils/Core/MagicalPdfConverter.java
+++ MagicalPEC/src/main/java/ir/vasl/magicalpec/utils/Core/MagicalPdfConverter.java
@@ -58,4 +58,39 @@ public class MagicalPdfConverter {
             throw new MagicalException(e.getMessage());
         }
     }
+
+    public String convertImageIntoPDF(Context context, String savePdfDestination, String imagePath) throws MagicalException {
+
+        if (savePdfDestination == null || savePdfDestination.isEmpty())
+            throw new MagicalException("Save PDF file distance is not valid");
+
+        if (imagePath == null)
+            throw new MagicalException("Image Path is not valid");
+
+        try {
+
+            File file = new File(savePdfDestination);
+            if (!file.exists()) {
+                String parent = file.getParent();
+                if (parent != null)
+                    new File(parent).mkdirs();
+            }
+
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream(savePdfDestination));
+            document.open();
+            Image image = Image.getInstance(imagePath);
+
+            float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
+                    - document.rightMargin() - 0) / image.getWidth()) * 100; // 0 means you have no indentation. If you have any, change it.
+            image.scalePercent(scaler);
+            image.setAlignment(Image.ALIGN_CENTER | Image.ALIGN_TOP);
+
+            document.add(image);
+            document.close();
+            return savePdfDestination;
+        } catch (Exception e) {
+            throw new MagicalException(e.getMessage());
+        }
+    }
 }
